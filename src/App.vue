33965d0114c72225acf8c620730ec457124b5f3e
Bruno Melo <brunonobrega.melo@gmail.com>
2017-08-21 20:39:01 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo"></img>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'AluraPic',
+      fotos: []
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos,
+      err => console.log(err));
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
